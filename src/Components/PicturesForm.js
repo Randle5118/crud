f072d46c862380e2af2399276a61123824b04ad4
Randle <>
2018-09-12 08:48:55 +0900

    add proof

--- src/Components/PicturesForm.js
+++ src/Components/PicturesForm.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
 class PictureForm extends Component {
   state ={
     title : '',
-    cover : ''
+    cover : '',
+    errors : {}
   }
 
   // e is mean event
+  // Object.assingn() is same like clone
   handleChange = (e) =>{
-    this.setState({ [e.target.name]: e.target.value })
+    if(!!this.state.errors[e.target.name]){
+    //  Use Object.assign() can keep the "this.state.errors" and just change the "errors"
+    let errors = Object.assign({}, this.state.errors);
+    delete errors[e.target.name];
+    this.setState({ 
+      [e.target.name]: e.target.value 
+    });
+    }else{
+      this.setState({ 
+        [e.target.name]: e.target.value 
+      })
+    }
   }
 
   handleSubmit = (e) =>{
     e.preventDefault();
+
+    let errors = {};
+    if (this.state.title === '') errors.title = "Title can't be empty"
+    if (this.state.cover === '') errors.cover = "Cover can't be empty"
+    this.setState( { errors } )
   }
 
   render() { 
@@ -20,8 +39,9 @@ class PictureForm extends Component {
       // onSubmit is let the button useful 
       <form className="ui form" onSubmit={ this.handleSubmit }>
         <h1>Add New Picture</h1>
-
-        <div className="field">
+        {/* // "!!" is let "this.state.errors.title" change to boolean
+               if "this.state.errors.title" is ture then errors will be use */}
+        <div className= { classnames('field',  { error: !!this.state.errors.title })}>
           <label htmlFor="title">Title</label>
           <input 
           type="text" 
@@ -29,9 +49,10 @@ class PictureForm extends Component {
           volue={ this.state.title }
           onChange={ this.handleChange }
           />
+          <span>{ this.state.errors.title }</span>
         </div>
 
-        <div className="field">
+        <div className= { classnames('field',  { error : !!this.state.errors.cover  })}>
           <label htmlFor="title">Cover</label>
           <input 
           type="text" 
@@ -39,6 +60,7 @@ class PictureForm extends Component {
           volue={ this.state.cover }
           onChange={ this.handleChange }
           />
+          <span>{ this.state.errors.cover }</span>
         </div>
 
         <div className="field">
